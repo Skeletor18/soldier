151b34c719ec2bbf2eed23c8893e98df608abbe4
Akhmed <ahmedchahkiev001@gmail.com>
2022-08-31 10:26:36 +0300

    Создал солдата

---
+++ soldier.js
@@ -0,0 +1,22 @@
+let soldier = {
+    name: 'Ragnar',
+    health: 5,
+    weapon:{
+       nameWeapon: 'M416',
+       cartridges: '30'
+    },
+    cartridges: 3,
+   
+    fire(){
+
+    },
+
+    recharge(){
+
+    },
+
+    hurt(){
+
+    },
+
+}
