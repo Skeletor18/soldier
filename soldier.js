e7b5f745825be287da1b4f739c249ec23f74ee65
Akhmed <ahmedchahkiev001@gmail.com>
2022-08-31 11:00:31 +0300

    Я создал солдата

--- soldier.js
+++ soldier.js
@@ -1,37 +1,36 @@
 let soldier = {
-    name: 'Ragnar',
-    health: 5,
-    weapon:{
-       nameWeapon: 'M416',
-       cartridges: 30
-    },
-    supplies: 3,
-   
-    fire(){
-        if (this.weapon.cartridges <= 0) {
-            console.log('обойма пуста. Перезаредитесь')
-        }else{
-            this.weapon.cartridges--
-            console.log(`бах бах`)
-        }
-    },
+  name: "Ragnar",
+  health: 5,
+  weapon: {
+    nameWeapon: "M416",
+    cartridges: 30,
+  },
+  supplies: 3,
 
-    recharge(){
-        if(this.supplies <= 0){
-            return console.log('Не осталось припасов')
-        }else{
-            this.weapon.cartridges = 30
-            this.supplies--
-            console.log('Перезарядка..') 
-        }
-    },
+  fire() {
+    if (this.weapon.cartridges <= 0) {
+      console.log("обойма пуста. Перезаредитесь");
+    } else {
+      this.weapon.cartridges--;
+      console.log(`бах бах`);
+    }
+  },
 
-    hurt(){
-        if (this.health <= 0) {
-           return console.log('Ты проиграл')
-        }else{
-            this.health--
-        }
-    },
+  recharge() {
+    if (this.supplies <= 0) {
+      return console.log("Не осталось припасов");
+    } else {
+      this.weapon.cartridges = 30;
+      this.supplies--;
+      console.log("Перезарядка..");
+    }
+  },
 
-}
+  hurt() {
+    if (this.health <= 0) {
+      return console.log("Ты проиграл");
+    } else {
+      this.health--;
+    }
+  },
+};
